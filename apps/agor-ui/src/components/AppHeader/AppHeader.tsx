793ae52d4fb6757f0b236be48cf9e30f2de7b328
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2026-08-14 22:20:07 -0400

    feat(ui): add idle signal-field screensaver

--- apps/agor-ui/src/components/AppHeader/AppHeader.tsx
+++ apps/agor-ui/src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,5 @@
 import type { ActiveUser, AgorClient, Board, BoardID, Branch, User } from '@agor-live/client';
-import { BulbOutlined } from '@ant-design/icons';
+import { BulbOutlined, PlayCircleOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Button, Divider, Layout, Popover, Space, Tag, Tooltip, theme } from 'antd';
 import { memo, useMemo } from 'react';
@@ -16,6 +16,7 @@ import { BrandLogo } from '../BrandLogo';
 import { BrandMark } from '../BrandMark';
 import { ConnectionStatus } from '../ConnectionStatus';
 import { GlobalUserMenu } from '../GlobalUserMenu';
+import { startIdleGlyphScreensaver } from '../IdleGlyphScreensaver';
 import { MarkdownRenderer } from '../MarkdownRenderer';
 import { buildThemeMenuItems } from '../ThemeSwitcher';
 import { AppHeaderGlobalSearch } from './AppHeaderGlobalSearch';
@@ -182,6 +183,12 @@ const AppHeaderInner: React.FC<AppHeaderProps> = ({
       key: 'theme',
       label: 'Theme',
       children: buildThemeMenuItems(themeMode, setThemeMode, onThemeEditorClick),
+    },
+    {
+      key: 'screensaver',
+      label: 'Start screensaver',
+      icon: <PlayCircleOutlined />,
+      onClick: startIdleGlyphScreensaver,
     },
     { type: 'divider' as const },
     {
