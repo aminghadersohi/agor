d0db8b0cccc1b0d99af6ad7daa43d377e62d378b
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2026-08-26 14:46:09 -0400

    test(ui): make async authority tests deterministic

--- apps/agor-ui/src/components/SettingsModal/AgenticToolPresetsManager.test.tsx
+++ apps/agor-ui/src/components/SettingsModal/AgenticToolPresetsManager.test.tsx
@@ -36,7 +36,9 @@ describe('AgenticToolPresetsManager authority lifetime', () => {
     fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'A reconnect draft' } });
     fireEvent.click(screen.getByRole('button', { name: 'OK' }));
     await waitFor(() => expect(create).toHaveBeenCalledOnce());
-    expect(screen.getByRole('button', { name: /OK$/ })).toHaveClass('ant-btn-loading');
+    await waitFor(() =>
+      expect(screen.getByRole('button', { name: /OK$/ })).toHaveClass('ant-btn-loading')
+    );
 
     rendered.rerender(view(2));
     await waitFor(() => expect(find).toHaveBeenCalledTimes(2));
